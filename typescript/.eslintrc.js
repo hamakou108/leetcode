ffb75f83a3a065b2ec171b7f20e2aa680c735f57
hamakou108 <hamakou108@gmail.com>
2021-03-21 14:48:42 +0900

    build: Add ESLint

---
+++ typescript/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json']
+  },
+  rules: {
+    'indent': ['error', 2]
+  }
+}
